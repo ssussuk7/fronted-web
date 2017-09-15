a5f82d60c76f5b9856b5e4e5d4f271d27e149775
ssussuk7 <ssussuk7@gmail.com>
2017-09-15 17:03:17 +0900

    02

---
+++ 2/02.js
@@ -0,0 +1,20 @@
+var str = 'hello world';
+console.log(str)
+console.log(1 + 2);
+
+var isMan = true;
+
+if(isMan)
+{
+  console.log('남자다');
+}
+
+else
+{
+  console.log('여자다');
+}
+
+for (var i = 0; i < 5; i++)
+{
+  console.log(i);
+}
