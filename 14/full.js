9dedaaddfc510a9d53fe8c89730affefa06423a7
ssussuk7 <ssussuk7@gmail.com>
2017-12-03 00:56:46 +0900

    14th

---
+++ 14/full.js
@@ -0,0 +1,22 @@
+function fullScreen(){
+  if (screenfull.enabled) {
+  	screenfull.request( document.querySelector('img') );
+  }
+
+}
+
+
+function getGeo(){
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+
+    document.getElementById('log').innerHTML =
+     'http://map.daum.net/link/map/여기,'+position.coords.latitude+','+position.coords.longitude;
+    //console.log(position.coords.latitude, position.coords.longitude);
+  });
+
+}
+
+document.getElementById('fullBtn').addEventListener('click', fullScreen);
+
+document.getElementById('geoBtn').addEventListener('click', getGeo);
